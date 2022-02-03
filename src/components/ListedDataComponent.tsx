ef661056429aac51c3d9cdaac4ef3a0519c403cd
Favious <faviopaco@gmail.com>
2022-02-03 00:50:04 -0400

    Add functionallity to render element cards

--- src/components/ListedDataComponent.tsx
+++ src/components/ListedDataComponent.tsx
@@ -1,5 +1,44 @@
-const ListedDataComponent = () => {
-  return <div>...</div>;
+import ITunesElementType from "../types/iTunesElement.type";
+
+type Props = {
+  iTunesElements: Array<ITunesElementType>;
+};
+
+const ListedDataComponent = (props: Props) => {
+  return (
+    <div className="row mt-3">
+      {props &&
+        !(props.iTunesElements.length === 0) &&
+        props.iTunesElements.map((element: ITunesElementType) => (
+          <div className="bg-light m-1 border-dark">
+            <div className="row">
+              <div className="col-4">
+                <div>
+                  <img
+                    src={element.artworkUrl100}
+                    width="200px"
+                    height="200px"
+                  />
+                </div>
+              </div>
+              <div className="col-8">
+                <div className="row">
+                  <h3>{element.collectionCensoredName || element.trackName}</h3>
+                </div>
+                <div>
+                  <h6>{element.artistName}</h6>
+                </div>
+                {element.collectionPrice && (
+                  <div>
+                    <h4 className="text-danger">{element.collectionPrice} $</h4>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default ListedDataComponent;
